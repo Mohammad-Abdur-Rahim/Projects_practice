249da94173d342195cbf4215c93451aebb40d0b9
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-15 22:14:36 +0600

    ***Triangle Dynamic FN(DONE)***

--- 1. geometry card/js/minimizeinputfn.js
+++ 1. geometry card/js/minimizeinputfn.js
@@ -1,14 +1,16 @@
 
 
 
-
+//2nd  call the main function calculate the value  --------
 function calculateTriangle(){
     const base =getBase('triangle-base');
     console.log(base);
     const height = getBase('triangle-height');
     console.log(height);
+    const result =0.5 *base *height;
+     console.log("Triangle value is :",result);
    }
-
+//1st Receive the triangle all input value & return------
 function getBase(inputFieldId) {
     const inputBase = document.getElementById(inputFieldId).value;
     const baseInput=parseFloat(inputBase);
